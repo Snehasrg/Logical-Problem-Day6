0e1e6d5cae95c0e6626b5bcce236d4e15fc55b52
Snehasrg <sghongade642@gmail.com>
2023-01-02 00:41:37 +0530

    Prime Number

---
+++ PrimeNumber.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Logial_Problem
+{
+    public class PrimeNumber
+    {
+        public static void checkprimeno()
+        {
+            Console.WriteLine("Enter the Number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("num is prime number");
+            }
+            else
+            {
+                Console.WriteLine("num is not a prime number");
+            }
+        }
+    }
+}
