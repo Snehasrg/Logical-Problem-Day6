3875682901f42397a90786c99d09c34bf83ed8b1
Snehasrg <sghongade642@gmail.com>
2023-01-02 00:35:00 +0530

    Logical Program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Logial_Problem;
+
+namespace LogialProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //FibonacciSeries.checkfibonacci();
+            // PerfectNumber.checkperfectno();
+            //ReverseNumber.checkreverseno();
+            // PrimeNumber.checkprimeno();
+            Coupon.checkcoupon();
+          
+        }
+    }
+}
