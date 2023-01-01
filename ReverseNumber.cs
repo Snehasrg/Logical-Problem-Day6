d19521301c4e35c9c7ed56e7683f8834ac461196
Snehasrg <sghongade642@gmail.com>
2023-01-02 00:40:07 +0530

    Reverse Number

---
+++ ReverseNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace Logial_Problem
+{
+    public class ReverseNumber
+    {
+        public static void checkreverseno()
+        {
+            int Reverse = 0;
+            Console.WriteLine("Enter the number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                int Remainder = num % 10;
+                Reverse = Reverse * 10 + Remainder;
+                num = num / 10;
+            }
+            Console.WriteLine(Reverse);
+        }
+    }
+}
