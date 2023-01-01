4886d3287743068e40569409a1dd5a5710e36783
Snehasrg <sghongade642@gmail.com>
2023-01-02 00:37:22 +0530

    Fibonaccis

---
+++ FibonacciSeries.cs
@@ -0,0 +1,22 @@
+﻿namespace Logial_Problem
+{
+    public class FibonacciSeries
+    {
+        public static void checkfibonacci()
+        {
+            Console.WriteLine("Enter Number of element:");
+            int num=Convert.ToInt32(Console.ReadLine());
+            int num1 = 0;
+            int num2=1;
+            Console.WriteLine(num1 + "" + num2 + "");
+            for (int i=0; i < num; i++)
+            {
+                int num3 = num1 + num2;
+                Console.WriteLine(num3 + "");
+                num1 = num2;
+                num2= num3;
+
+            }
+        }
+    }
+}
