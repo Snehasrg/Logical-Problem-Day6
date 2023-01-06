52e32cc1b5f8c8c26f35e9f23466410b12890a14
Snehasrg <sghongade642@gmail.com>
2023-01-06 15:29:35 +0530

    Coupon Program

---
+++ Coupon.cs
@@ -0,0 +1,39 @@
+﻿namespace Logial_Problem
+{
+    public class Coupon
+    {
+        public static void checkcoupon()
+        {
+
+            Console.WriteLine("\n***************Distnict Cuoepn number****************\n ");
+            Console.WriteLine("enter a number of distinct values: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] Arr = new int[n];
+            int i = 1;
+            int count = 0;
+            var random = new Random();
+            int j = 0;
+            Console.WriteLine();
+
+            for (i = 1; i < n; i++)
+            {
+                var random_number = random.Next(1, n);
+                if (Arr.Contains(random_number))
+                {
+                    i--;
+                }
+                else
+                {
+                    Arr[i] = random_number;
+                }
+                count++;
+
+            }
+            Console.WriteLine("COUNT IS :{0} ", count);
+            for (j = 0; j < Arr.Length; j++)
+            {
+                Console.WriteLine("array is:{0} ", Arr[j]);
+            }
+        }
+    }
+}
