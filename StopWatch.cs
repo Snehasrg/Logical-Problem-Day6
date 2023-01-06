d0fce30b959b810aa42e49f25886971ecd847118
Snehasrg <sghongade642@gmail.com>
2023-01-06 15:48:56 +0530

    Stop Watch Program

---
+++ StopWatch.cs
@@ -0,0 +1,18 @@
+﻿namespace Logial_Problem
+{
+    public class StopWatch
+    {
+        public static void Check()
+        { 
+            Console.WriteLine("Press Enter to Start.");
+            Console.ReadLine();
+            DateTime Start = DateTime.Now;
+
+            Console.WriteLine("Press Enter to Stop.");
+            Console.ReadLine();
+            DateTime Stop = DateTime.Now;
+
+            Console.Write("Elapsed Time = " + (Stop - Start));
+        }
+    }
+}
